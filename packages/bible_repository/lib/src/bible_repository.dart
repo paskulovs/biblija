6d5dfc154c74195f0c7e8002f31da87e5bc679d4
Sinisa Paskulov <paskulov.sinisa@icloud.com>
2024-04-26 16:36:38 +0200

    Bible repository refactoring

--- packages/bible_repository/lib/src/bible_repository.dart
+++ packages/bible_repository/lib/src/bible_repository.dart
@@ -1,22 +1,5 @@
-import 'dart:async';
-import 'dart:convert';
-import 'package:flutter/services.dart' show rootBundle;
+import '../bible_repository.dart';
 
-import 'model/bible_model.dart';
-
-class BibleRepository {
-
-  Future<BibleModel?> readJsonFile(String filePath) async {
-    final bibleString = await rootBundle.loadString(filePath);
-    if(bibleString.isEmpty) {
-      return null;
-    }
-
-    var bibleJson = jsonDecode(bibleString);
-    if(bibleJson == null || bibleJson.isEmpty || !(bibleJson is Map<String, dynamic>)) {
-      return null;
-    }
-
-    return BibleModel.fromJson(bibleJson);
-  }
+abstract class BibleRepository {
+  Future<BibleModel> loadBible();
 }
